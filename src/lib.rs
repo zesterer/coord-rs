5cde8853900c0e2d4875fb3901d10c5d08c3821b
johannesvollmer <johannes.boenki@t-online.de>
2018-07-08 16:44:19 +0200

    VecDot: Added tests for vec2 and vec3, and inserted use::VecDot into prelude

--- src/lib.rs
+++ src/lib.rs
@@ -204,6 +204,7 @@ pub mod prelude {
     pub use super::vec4::Vec4;
 
     pub use math::VecNum;
+    pub use math::VecDot;
     pub use math::VecInt;
     pub use math::VecUnsigned;
     pub use math::VecSigned;
@@ -341,6 +342,36 @@ mod tests {
         pass_vec2u(vec2!(1, 2));
         pass_vec3u(vec3!(1, 2, 3));
         pass_vec4u(vec4!(1, 2, 3, 4));
+    }
+
+    #[test]
+    fn dot_product_vec2(){
+        let zero = vec2!(0, 0);
+        let one = vec2!(1, 1);
+        let right = vec2!(1, 0);
+        let left = vec2!(-1, 0);
+        let up = vec2!(0, 1);
+
+        assert_eq!(zero.dot(zero), 0);
+        assert_eq!(one.dot(one), 2);
+        assert_eq!(right.dot(up), 0);
+        assert_eq!(right.dot(right), 1);
+        assert_eq!(right.dot(left), -1);
+    }
+
+    #[test]
+    fn dot_product_vec3(){
+        let zero = vec3!(0, 0, 0);
+        let one = vec3!(1, 1, 1);
+        let right = vec3!(1, 0, 0);
+        let left = vec3!(-1, 0, 0);
+        let up = vec3!(0, 1, 0);
+
+        assert_eq!(zero.dot(zero), 0);
+        assert_eq!(one.dot(one), 3);
+        assert_eq!(right.dot(up), 0);
+        assert_eq!(right.dot(right), 1);
+        assert_eq!(right.dot(left), -1);
     }
 
     fn length_of<V: VecFloat>(vec: V) -> V::Item where V::Item: math::Float {
